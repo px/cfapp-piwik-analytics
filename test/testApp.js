a0fc133f182d2434344463783522a90dc1d527d3
px <px@ns1.net>
2013-01-21 21:50:59 -0800

    timer update

--- test/testApp.js
+++ test/testApp.js
@@ -1,6 +1,7 @@
-document = document || [];
+"use strict";
 
-"use strict;";
+// document = document || [];
+
 
 var _debug = _debug || true;
 
@@ -12,28 +13,27 @@ var get_tracker="default tracker";
 _paq.push([ function() { visitor_id = this.getVisitorId(); }]);
 // _paq.push([ function() { get_tracker = this.getAsyncTracker(); }]);
 
-function update_status() {
-
+function update_status(document, piwikConfig) {
   document.getElementById("visitorId").innerHTML = "getVisitorId="+ visitor_id ;
   document.getElementById("timeDiv").innerHTML = "Timer update";
 
   try {
 
-  document.getElementById("piwik_js_default").innerHTML = "piwik_js_default="+piwikConfig.piwik_js_default;
-  document.getElementById("js_url").innerHTML = "js_url="+piwikConfig.js_url;
-  document.getElementById("use_cdnjs").innerHTML = "use_cdnjs="+ piwikConfig.use_cdnjs ;
-  
-  
-  document.getElementById("site_id.a").innerHTML = "site_id.a="+piwikConfig.site_id.a;
-  document.getElementById("site_id.b").innerHTML = "site_id.b="+piwikConfig.site_id.b;
-  
-  document.getElementById("trackerURL.a").innerHTML = "trackerURL.a="+piwikConfig.tracker.a;
-  document.getElementById("trackerURL.b").innerHTML = "trackerURL.b="+piwikConfig.tracker.b;
-  
-  document.getElementById("getTracker").innerHTML = "getTracker="+get_tracker;
+    document.getElementById("piwik_js_default").innerHTML = "piwik_js_default="+piwikConfig.piwik_js_default;
+    document.getElementById("js_url").innerHTML = "js_url="+piwikConfig.js_url;
+    document.getElementById("use_cdnjs").innerHTML = "use_cdnjs="+ piwikConfig.use_cdnjs ;
 
-  document.getElementById("paq_push.a").innerHTML = "paq_push.a="+piwikConfig.paq_push.a;
-  document.getElementById("paq_push.b").innerHTML = "paq_push.b="+piwikConfig.paq_push.b;
+
+    document.getElementById("site_id.a").innerHTML = "site_id.a="+piwikConfig.site_id.a;
+    document.getElementById("site_id.b").innerHTML = "site_id.b="+piwikConfig.site_id.b;
+
+    document.getElementById("trackerURL.a").innerHTML = "trackerURL.a="+piwikConfig.tracker.a;
+    document.getElementById("trackerURL.b").innerHTML = "trackerURL.b="+piwikConfig.tracker.b;
+
+    document.getElementById("getTracker").innerHTML = "getTracker="+get_tracker;
+
+    document.getElementById("paq_push.a").innerHTML = "paq_push.a="+piwikConfig.paq_push.a;
+    document.getElementById("paq_push.b").innerHTML = "paq_push.b="+piwikConfig.paq_push.b;
   } catch (e) {
 
     /* silently ignore the error */
@@ -41,40 +41,13 @@ function update_status() {
 
 }
 
-// 
-var limit="0:3";
+var timerSec=0.5;
 
-if(document.images){var parselimit=limit.split(":");parselimit=parselimit[0]*60+parselimit[1]*1;}
+var myVar = setInterval( function() { myTimer(); } , timerSec*1000 );
 
-var saveTime=parselimit;
 
-function refresh(){
-  if(!document.images){return;}
-  if(parselimit==1){
-
-    // do work
-    update_status();
-
-    parselimit=saveTime;
-    setTimeout(refresh,1000);
-
-  } else {
-
-    parselimit-=1;
-    curmin=Math.floor(parselimit/60);
-    cursec=parselimit%60;
-    if(curmin!==0){
-      curtime=curmin+" minutes and "+cursec+" seconds until update";
-    } else {
-      curtime=cursec+" seconds left until update";
-    }
-
-    document.getElementById('timeDiv').innerHTML = curtime;
-
-    // unquoted causes stack error when using arguments ()
-    setTimeout(refresh,1000);
-  }
+function myTimer()
+{
+  update_status(document, piwikConfig);
 }
-
-window.onload=refresh();
 
