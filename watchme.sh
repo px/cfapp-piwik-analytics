0af12b484726da4999f349d76494f222220420da
px <px@ns1.net>
2013-02-02 00:15:07 -0800

    watcher

--- watchme.sh
+++ watchme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-coffee --watch --lint --bare --join piwik_analytics.js --output public/javascripts/ src/coffeescripts/piwik_analytics-hot.coffee
+coffee --watch --lint --bare --output public/javascripts/ src/coffeescripts/
 
-coffee --watch --compile --bare test/testApp.coffee
+# coffee --watch --compile --bare test/testApp.coffee
 
 
