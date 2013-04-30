f525b0eb9487e7a5fdd9bf7f9e831845fa57bfb9
px <px@ns1.net>
2013-04-30 13:42:46 -0700

    updates

--- watchme.sh
+++ watchme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-coffee --watch --lint --bare --output public/javascripts/ src/coffeescripts/
+coffee --watch --lint --bare --map --output public/javascripts/ src/coffeescripts/
 
 # coffee --watch --compile --bare test/testApp.coffee
 
