f32a005c44c46d1a9662ec3cd20a4da8001885e0
px <px@ns1.net>
2013-05-05 22:39:51 -0700

    output a minified version

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ _FILE=public/javascripts/piwik_analytics.js
 
 find ./ -type f -name \*.js -exec jsl -process {} \;
 
-
+jsmin <public/javascripts/piwik_analytics.js >public/javascripts/piwik_analytics.min.js
 echo copy over in place cloudflare.json
 #find ./ -type f -name \*.json -exec jsonlint-i {} \;
 
