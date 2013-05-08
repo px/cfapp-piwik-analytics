b56871b0c2273412b2af2f3764207cf8156ee6a8
px <px@ns1.net>
2013-05-07 21:28:27 -0700

    this script is on the way out soon.

--- validate.sh
+++ validate.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -x
 
+# clunky but works :)
+
 _FILE=public/javascripts/piwik_analytics.js
 
 find ./ -type f -name \*.js -exec jsl -process {} \;
 
-jsmin <public/javascripts/piwik_analytics.js >public/javascripts/piwik_analytics.min.js
+# minify and concatenate with the header
+cat public/javascripts/header.js >public/javascripts/piwik_analytics.min.js
+jsmin <public/javascripts/piwik_analytics.js >>public/javascripts/piwik_analytics.min.js
+
 echo copy over in place cloudflare.json
 #find ./ -type f -name \*.json -exec jsonlint-i {} \;
 
