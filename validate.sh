8060afd4c5b9cdceb913faa9eb27a9be8338bc9f
px <px@ns1.net>
2013-05-25 12:26:19 -0700

    only do cloudflare.json, move into Cakefile soon please

--- validate.sh
+++ validate.sh
@@ -5,11 +5,11 @@ set -x
 
 _FILE=public/javascripts/piwik_analytics.js
 
-find ./ -type f -name \*.js -exec jsl -process {} \;
+#find ./ -type f -name \*.js -exec jsl -process {} \;
 
 # minify and concatenate with the header
-cat public/javascripts/header.js >public/javascripts/piwik_analytics.min.js
-jsmin <public/javascripts/piwik_analytics.js >>public/javascripts/piwik_analytics.min.js
+#cat public/javascripts/header.js >public/javascripts/piwik_analytics.min.js
+#jsmin <public/javascripts/piwik_analytics.js >>public/javascripts/piwik_analytics.min.js
 
 echo copy over in place cloudflare.json
 #find ./ -type f -name \*.json -exec jsonlint-i {} \;
